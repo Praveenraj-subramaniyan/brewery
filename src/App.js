b71b2d91ace96af57bfbe5e7734ad20410dc7b7a
spr887011 <spr887011@gmail.com>
2023-04-26 21:06:33 +0530

    Ui and api fetch completed , need to do changes in search box

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [breweryData, setbreweryData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      fetch(`https://api.openbrewerydb.org/breweries/`)
+        .then((response) => response.json())
+        .then((data) => {
+          data.sort((a, b) => {
+            if (a.name.common < b.name.common) {
+              return -1;
+            }
+            if (a.name.common > b.name.common) {
+              return 1;
+            }
+            return 0;
+          });
+          setbreweryData(data);
+          console.log(data[0].name);
+        })
+        .catch((error) => console.log(error));
+    }
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="MainDiv">
+      <div className="bgimageDiv">
+        <div className="search-bar">
+          <input
+            type="text"
+            placeholder="Brewery name"
+            className="inputBox"
+          ></input>
+          <button >Search</button>
+        </div>
+      </div>
+      <div className="details">
+        <div>
+          {
+            breweryData.map((data) =>(
+          <div class="card">
+            <div class="container">
+              <p>
+                <b>{data.name} - {data.brewery_type}</b>
+              </p>
+              <p>{data.address_1},{data.city},{data.state_province},{data.country} - {data.postal_code} </p>
+              <a href={data.website_url}><b>Website :</b> {data.website_url}</a>
+              <p><b>Phone : </b>{data.phone}</p>
+            </div>
+          </div>
+          ))
+        }
+        </div>
+      </div>
     </div>
   );
 }
